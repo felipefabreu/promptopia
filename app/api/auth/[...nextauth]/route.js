5907aee1f7cfd3a4487b8c37ec6de6d0c35824e6
Felipe <felipefabreu@gmail.com>
2023-07-21 00:10:30 -0300

    adicionando conexao com mongo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,36 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      const sesstionUser = await User.findOne({ email: session.user.email });
 
-  async session({ session }) {
-    const sesstionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sesstionUser._id.toString();
 
-    session.user.id = sesstionUser._id.toString();
+      return session;
+    },
 
-    return session;
-  },
+    async signIn({ profile }) {
+      try {
+        await connectToDatabase();
 
-  async signIn({ profile }) {
-    try {
-      await connectToDatabase();
+        //check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      //check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
+        //if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-      //if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
